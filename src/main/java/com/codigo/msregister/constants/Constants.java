fe0d64d7d745c360cc071403df632254a93f4645
Abel Guevara <55357845+AbelG9@users.noreply.github.com>
2024-01-08 21:42:59 -0500

    Create Constants.java

---
+++ src/main/java/com/codigo/msregister/constants/Constants.java
@@ -0,0 +1,20 @@
+package com.codigo.msregister.constants;
+
+public class Constants {
+    //CODE
+    public static final Integer CODE_SUCCESS=200;
+    public static final Integer CODE_ERROR=400;
+
+    //MESSAGES
+    public static final String MESSAGE_SUCCESS="Ejecución correcta";
+    public static final String MESSAGE_ERROR="Error en la Ejecución";
+    public static final Integer LENGTH_RUC=15;
+    public static final Integer LENGTH_DNI=8;
+
+    //STATUS
+    public static final Integer STATUS_ACTIVE=1;
+    public static final Integer STATUS_INACTIVE=0;
+
+    //AUDIT
+    public static final String AUDIT_ADMIN="PRODRIGUEZ";
+}
